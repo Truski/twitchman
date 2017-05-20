fe6cfd2a23fb82b69821169f39e7f4c438e71c2e
Daniel Wojtowicz <dwojtowicz20@gmail.com>
2017-05-20 14:28:43 -0500

    Added header, footer, and custom css

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+</div>
+</div>
+<script type="text/javascript" src="/js/jquery-3.2.0.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="A Twitch Stream Manager">
+<meta name="author" content="Daniel Wojtowicz">
+<title><?=$title?></title>
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/css/custom.css">
+</head>
+<body>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-3 sidebar">
+<h2 class="text-center">Twitch Manager</h2>
+<ul class="list-group">
+<li class="list-group-item">Link 1</li>
+<li class="list-group-item">Link 1</li>
+</ul>
+<!-- <iframe frameborder="0" 
+        scrolling="no" 
+        id="chat_embed" 
+        src="http://www.twitch.tv/vjasmash/chat" 
+        height="725" 
+        width="100%">
+</iframe> -->
+<div class="twitchplaceholder">
+<p>Twitch Chat Placeholder</p>
+</div>
+</div>
