74a95e8de29751aa1b824838f37384975ca4fccf
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-01-01 21:36:23 -0600

    Add character and commentator selection areas

--- application/views/default.php
+++ application/views/default.php
@@ -103,3 +103,45 @@
 
 	</div>
 </div>
+
+<img id="charselect" src="/assets/img/charlist.png" border="0" width="1164" height="370" usemap="#charmap" alt="" />
+<map name="charmap" id="charmap">
+<area class="chararea" alt="drmario" title="" href="#" shape="rect" coords="6,10,127,129" />
+<area class="chararea" alt="mario" title="" href="#" shape="rect" coords="136,3,257,122" />
+<area class="chararea" alt="luigi" title="" href="#" shape="rect" coords="264,4,385,123" />
+<area class="chararea" alt="bowser" title="" href="#" shape="rect" coords="392,4,513,123" />
+<area class="chararea" alt="peach" title="" href="#" shape="rect" coords="520,3,641,122" />
+<area class="chararea" alt="yoshi" title="" href="#" shape="rect" coords="650,1,771,120" />
+<area class="chararea" alt="donkeykong" title="" href="#" shape="rect" coords="779,1,900,120" />
+<area class="chararea" alt="falcon" title="" href="#" shape="rect" coords="905,3,1026,122" />
+<area class="chararea" alt="ganon" title="" href="#" shape="rect" coords="1033,9,1154,128" />
+<area class="chararea" alt="falco" title="" href="#" shape="rect" coords="7,131,128,250" />
+<area class="chararea" alt="fox" title="" href="#" shape="rect" coords="133,125,254,244" />
+<area class="chararea" alt="ness" title="" href="#" shape="rect" coords="262,124,383,243" />
+<area class="chararea" alt="icees" title="" href="#" shape="rect" coords="390,123,511,242" />
+<area class="chararea" alt="kirby" title="" href="#" shape="rect" coords="520,124,641,243" />
+<area class="chararea" alt="samus" title="" href="#" shape="rect" coords="648,122,769,241" />
+<area class="chararea" alt="zelda" title="" href="#" shape="rect" coords="777,124,837,243" />
+<area class="chararea" alt="sheik" title="" href="#" shape="rect" coords="837,124,898,243" />
+<area class="chararea" alt="link" title="" href="#" shape="rect" coords="905,126,1026,245" />
+<area class="chararea" alt="younglink" title="" href="#" shape="rect" coords="1034,134,1155,253" />
+<area class="chararea" alt="pichu" title="" href="#" shape="rect" coords="136,249,257,368" />
+<area class="chararea" alt="pikachu" title="" href="#" shape="rect" coords="263,247,384,366" />
+<area class="chararea" alt="jigglypuff" title="" href="#" shape="rect" coords="389,247,510,366" />
+<area class="chararea" alt="mewtwo" title="" href="#" shape="rect" coords="521,246,642,365" />
+<area class="chararea" alt="gaw" title="" href="#" shape="rect" coords="648,245,769,364" />
+<area class="chararea" alt="marth" title="" href="#" shape="rect" coords="774,247,895,366" />
+<area class="chararea" alt="roy" title="" href="#" shape="rect" coords="904,249,1025,368" />
+</map>
+
+<img id="commselect" src="/assets/img/bteam.jpg" border="0" width="1100" height="325" usemap="#commmap" alt="" />
+<map name="commmap" id="commmap">
+<area class="commarea" alt="0" title="" href="#" shape="rect" coords="0,0,133,277" />
+<area class="commarea" alt="1" title="" href="#" shape="rect" coords="138,0,271,277" />
+<area class="commarea" alt="2" title="" href="#" shape="rect" coords="280,0,413,277" />
+<area class="commarea" alt="3" title="" href="#" shape="rect" coords="417,0,550,277" />
+<area class="commarea" alt="4" title="" href="#" shape="rect" coords="553,0,686,277" />
+<area class="commarea" alt="5" title="" href="#" shape="rect" coords="691,0,824,277" />
+<area class="commarea" alt="6" title="" href="#" shape="rect" coords="828,0,961,277" />
+<area class="commarea" alt="7" title="" href="#" shape="rect" coords="967,0,1100,277" />
+</map>
