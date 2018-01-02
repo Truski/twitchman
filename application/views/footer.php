e0ca5ced11ed2607839683a9921696403a78d1f6
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-01-01 21:32:58 -0600

    Add Chart.js

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,6 +3,7 @@
 	</div>
 	<script type="text/javascript" src="/js/jquery-3.2.0.min.js"></script>
 	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="/js/Chart.bundle.min.js"></script>
 	<script type="text/javascript" src="/js/custom.js"></script>
 	</body>
 </html>
